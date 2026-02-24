2b8693d7754e9638c95fcabd582af9f2fe9dbce3
Sriram S Nambiar <sriramnambiar21@gmail.com>
2026-02-24 16:50:41 +0530

    database connection function
    Added a comment to clarify the purpose of the connectDB function.

--- src/db/index.js
+++ src/db/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
 
-
+//Connect to database
 const connectDB = async () => {
     try {
         const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
